705a11470a0833c5bcb754a2f85eace33c67567c
Rasti1 <rostrostov1999@gmail.com>
2024-03-11 10:59:54 +0200

    Incorrect project

---
+++ src/app/Product.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention()
+public @interface Product {
+    String name() default "unknown";
+    int quota() default;
+}
